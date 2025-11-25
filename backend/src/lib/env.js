e7827695dfe336e0b211937d286c82e36d07b467
shahnawaz khan <khan.dev006@gmail.com>
2025-11-25 16:43:04 +0530

    added deployement config

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,6 +1,8 @@
- 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT || 3000;
-export const MONGODB_URI = process.env.MONGODB_URI;
+export const ENV = {
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT || 3000,
+  MONGODB_URI: process.env.MONGODB_URI,
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      hello world
+      i am srk from frontend
     </div>
   )
 }
