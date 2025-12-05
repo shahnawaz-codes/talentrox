e6a6c521beb96be29ceac62ae2404cfe622dfe07
shahnawaz khan <khan.dev006@gmail.com>
2025-12-05 12:49:53 +0530

    naming mismatch

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,10 +1,10 @@
 
 export const generateToken = (req, res, next) => {
   try {
-    const { clearkId, name, imageUrl } = req.user;
+    const { clerkId, name, imageUrl } = req.user;
     // Generate Stream token for the user based on their Clerk ID not database ID because we used Clerk ID in Stream
-    const token = chatClient.createToken(clearkId);
-    res.json({ token, user: { id: clearkId, name, image: imageUrl } });
+    const token = chatClient.createToken(clerkId);
+    res.json({ token, user: { id: clerkId, name, image: imageUrl } });
   } catch (error) {
     console.error("Error generating Stream token:", error);
     next(error);
