4ecd29f42640dc31f5fe96afed3178b742eafb16
shahnawaz khan <khan.dev006@gmail.com>
2026-04-01 13:30:50 +0530

    implemented everything related to stream and chat

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,10 +1,14 @@
+import { chatClient } from "../lib/stream.js";
 
 export const generateToken = (req, res, next) => {
   try {
     const { clerkId, name, imageUrl } = req.user;
     // Generate Stream token for the user based on their Clerk ID not database ID because we used Clerk ID in Stream
     const token = chatClient.createToken(clerkId);
-    res.json({ token, user: { id: clerkId, name, image: imageUrl } });
+    res.json({
+      token,
+      user: { userId: clerkId, userName: name, userImage: imageUrl },
+    });
   } catch (error) {
     console.error("Error generating Stream token:", error);
     next(error);
--- backend/src/index.js
+++ backend/src/index.js
@@ -11,13 +11,13 @@ import sessionRoutes from "./routes/sessionRoute.js";
 import errorHandler from "./midleware/errorHandler.js";
 
 const app = express();
-// Clerk middleware for authentication. it gives access to req.auth
+// Clerk middleware for authentication.it gives access to req.auth
 app.use(clerkMiddleware());
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // CORS configuration to allow requests from the client URL
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: ENV.CLIENT_URL || "*", credentials: true }));
 // Inngest setup for handling functions and events
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
